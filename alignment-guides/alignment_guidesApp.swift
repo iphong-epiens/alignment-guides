80da72321c07a00a7dac687fb3d03cebf70a1229
Inpyo Hong <y2kpaulh@epiens.com>
2021-01-09 22:32:00 +0900

    commit

---
+++ alignment-guides/alignment_guidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  alignment_guidesApp.swift
+//  alignment-guides
+//
+//  Created by Inpyo Hong on 2021/01/09.
+//
+
+import SwiftUI
+
+@main
+struct alignment_guidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
